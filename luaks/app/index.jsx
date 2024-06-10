830820e905ff7d28400c9afb3db2266ee9b804f3
lukasnielson <lnielson1@yahoo.com>
2024-06-10 16:42:32 -0500

    before trying to install nativewind

--- luaks/app/index.jsx
+++ luaks/app/index.jsx
@@ -1,15 +1,21 @@
-import { Text, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from "react-native";
+import { Link } from 'expo-router';
 
-export default function Index() {
+export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text>Lukas.</Text>
+      <StatusBar style="auto" />
+      <Link href="/profile" style={{color:'blue'}}>Go to Profile</Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+  }
+})
